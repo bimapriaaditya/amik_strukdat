fcd130862b8c0995fb3ddbff7ad55f7766c1ef98
bimapriaaditya <bpabima@gmail.com>
2022-11-30 23:43:36 +0700

    uncomment

--- src/App.java
+++ src/App.java
@@ -4,16 +4,16 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Tugas 1 : Bubble Sort
-        // BubbleSort Tugas1 = new BubbleSort();
-        // Tugas1.main();
+        BubbleSort Tugas1 = new BubbleSort();
+        Tugas1.main();
 
         // Tugas 2 : Selection Sort
-        // SelectionSort Tugas2 = new SelectionSort();
-        // Tugas2.main();
+        SelectionSort Tugas2 = new SelectionSort();
+        Tugas2.main();
 
         // Tugas 3 : Insertion Sort
-        // InsertionSort Tugas3 = new InsertionSort();
-        // Tugas3.main();
+        InsertionSort Tugas3 = new InsertionSort();
+        Tugas3.main();
 
         // Tugas 4 : Merge Sort
         MergeSort Tugas4 = new MergeSort();
